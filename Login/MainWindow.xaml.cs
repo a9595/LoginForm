cd13ab33c9a9ff89ffa31fe479f6e8d8cfac02cf
a9595 <andriy9595@mail.ru>
2013-10-25 14:46:18 +0200

    final

---
+++ Login/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static public List<Users> UsersList = new List<Users>(); //список логінів-паролей
+        private int LoginedIndex; //індекс залогіненого
+        private bool Logined; //чі залогінився
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadAllLoginsFromBase();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush b = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            buttonClose.Background = b;
+            Close();
+        }
+
+        private void ButtonRegClick1(object sender, RoutedEventArgs e)
+        {
+            Registration a = new Registration();
+            Close();
+            a.ShowDialog();
+
+        }
+
+        private void buttonEnter_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(textBoxLogin.Text) || String.IsNullOrWhiteSpace(passwordBoxPass.Password))
+            {
+                MessageBox.Show("Заповніть будь-ласка усі поля");
+                return;
+            }
+            for (int i = 0; i < UsersList.Count(); i++)
+            {
+                if(string.Compare(textBoxLogin.Text, UsersList[i].Login, StringComparison.OrdinalIgnoreCase) == 0 &&
+                    string.Compare(passwordBoxPass.Password, UsersList[i].Password, StringComparison.CurrentCulture) ==0)
+                {
+                    MessageBox.Show("Ви увійшли успішно!");
+                    Logined = true;
+                    break;
+                }
+                
+            }
+            if(!Logined)
+                MessageBox.Show("Невірний логін або пароль");
+        }
+
+        public static void WriteAllLoginsToBase()
+        {
+            if (!Directory.Exists(@"files"))//якшо не буде папкі чогось, то нову створить
+                Directory.CreateDirectory(@"files");
+
+            BinaryWriter writer = new BinaryWriter(File.Open(@"files\base.dat", FileMode.Create),
+                                                       Encoding.BigEndianUnicode);
+            foreach (Users login in UsersList)
+            {
+                writer.Write(login.Login);
+                writer.Write(login.Password);
+            }
+            writer.Close();
+        }
+        public static void ReadAllLoginsFromBase()
+        {
+            UsersList.Clear(); //очищаємо список, для уникнення дублікатів
+            try
+            {
+                BinaryReader reader = new BinaryReader(File.Open(@"files\base.dat", FileMode.Open), Encoding.BigEndianUnicode);
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    UsersList.Add(new Users(reader.ReadString(), reader.ReadString()));
+                }
+                reader.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Виникла помилка з файлом бази данних профілів");
+            }
+        }
+    }
+}
---
+++ Login/Registration.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// Interaction logic for Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowMouseLeftButtonDown1(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ButtonCloseClick(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush b = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            buttonClose.Background = b;
+            Close();
+        }
+
+        private void ButtonAcceptClick1(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxLogin.Text) ||
+                string.IsNullOrWhiteSpace(passwordBoxPass.Password) ||
+                string.IsNullOrWhiteSpace(passwordBoxPassAgain.Password))
+                MessageBox.Show("Заповніть будь-ласка УСІ поля");
+            else
+            {
+                if (passwordBoxPass.Password != passwordBoxPassAgain.Password)
+                {
+                    MessageBox.Show("Повторний пароль не вірний =(");
+                    passwordBoxPassAgain.Focus();
+                    passwordBoxPassAgain.SelectAll();
+                }
+                else
+                {
+                    Users tmp = new Users(textBoxLogin.Text, passwordBoxPass.Password);
+                    if (MainWindow.UsersList.Contains(tmp))
+                    {
+                        MessageBox.Show(string.Format("профіль  " + textBoxLogin.Text + " уже існує." +
+                                                  "\nСтворіть будь-ласка профіль з іншою назвою"));
+                        textBoxLogin.Clear();
+                        passwordBoxPass.Clear();
+                        passwordBoxPassAgain.Clear();
+                    }
+                    else
+                    {
+                        MainWindow.UsersList.Add(tmp);
+                        MainWindow.WriteAllLoginsToBase();
+                        MessageBox.Show(string.Format("Логін: {0} ", textBoxLogin.Text),
+                                    "Профіль створено успішно");
+                        MainWindow window = new MainWindow();
+                        Close();
+                        window.ShowDialog();
+                    }
+                }
+            }
+
+        }
+
+        private void ButtonCancelClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow tmp = new MainWindow();
+            Close();
+            tmp.ShowDialog();
+        }
+    }
+}
